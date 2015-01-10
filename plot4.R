fc6f170b8df789a6ae39094f39ab5046fbf40b26
Alexander L. Belikoff <abelikoff@gmail.com>
2015-01-09 22:36:49 -0500

    Finished

--- plot4.R
+++ plot4.R
@@ -20,10 +20,20 @@ if (file.exists(subset.file)) {
 
 ## generate plot
 
-png(filename = "plot3.png", width = 480, height = 480)
+png(filename = "plot4.png", width = 480, height = 480)
+par(mfrow = c(2, 2))
+plot(tbl$Time, tbl$Global_active_power, type="l",
+     xlab="", ylab="Global Active Power")
+
+plot(tbl$Time, tbl$Voltage, type="l",
+     xlab="datetime", ylab="Voltage")
+
 plot(tbl$Time, tbl$Sub_metering_1, type="l",
      xlab="", ylab="Energy sub metering")
 lines(tbl$Time, tbl$Sub_metering_2, col="red")
 lines(tbl$Time, tbl$Sub_metering_3, col="blue")
 legend("topright", lty = 1, col = c("black", "red", "blue"),
        legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"))
+
+plot(tbl$Time, tbl$Global_reactive_power, type="l",
+     xlab="datetime", ylab="Global_reactive_power")
